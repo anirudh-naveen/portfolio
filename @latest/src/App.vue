ffc1d9a30602a92dc6967bbaf142ec3437dea6fd
Anirudh Naveen <anirudhnaveen@outlook.com>
2025-10-13 21:59:53 -0400

    restart initial project creation

---
+++ @latest/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <AppNavbar />
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppNavbar from '@/components/AppNavbar.vue'
+</script>
+
+<style>
+body {
+  margin: 0;
+  background-color: #0d1912;
+  color: #f4e4c1;
+  font-family: "Poppins", sans-serif;
+}
+</style>
